58e450c1c464b09ce2be97bbc3306ae306dab9f4
ebrardev <16155052@mersin.edu.tr>
2025-01-04 18:01:11 +0300

    feat: Add updateTaskStatus endpoint to TasksController
    This commit adds a new endpoint `updateTaskStatus` to the `TasksController` in order to update the status of a specific task. The endpoint is implemented by calling the `updateTaskStatus` method of the `TasksService` and accepts the `id` as a route parameter and the `status` as a request body parameter.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { createTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -27,4 +27,8 @@ export class TasksController {
        return this.tasksService.deleteTask(id);
     }
     
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id') id:string, @Body('status') status: TaskStatus): Task {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
 }
