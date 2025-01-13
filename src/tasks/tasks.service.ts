8e7102fa26a31f4d30af6130ca3088a1a57a6ddf
ebrardev <16155052@mersin.edu.tr>
2025-01-13 23:50:45 +0300

    feat: Add error handling to getTaskById endpoint
    This commit adds error handling to the `getTaskById` endpoint in the `TasksService`. If a task with the specified ID is not found, a `NotFoundException` is thrown. This ensures that the API returns a proper error response when attempting to retrieve a non-existent task.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid} from 'uuid';
 import { createTaskDto } from './dto/create-task.dto';
@@ -26,7 +26,15 @@ export class TasksService {
     }
 
     getTaskById(id:string):Task{
-        return this.tasks.find(task=>task.id===id);
+
+
+
+        const found= this.tasks.find(task=>task.id===id);
+
+        if(!found){
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+        return found;
 
     }
     createTask(createTaskDto:createTaskDto):Task{
